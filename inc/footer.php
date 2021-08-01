26ff9261ee1171d8e3c789939e31ec33497ce26d
abdulweb <abdulrasheeda9@gmail.com>
2021-08-01 13:43:59 +0100

    dependecies set up

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+<!-- footer start -->
+			<div class="footer-wrap pd-20 mb-20 card-box">
+				Designed and Developed By Zahida
+			</div>
+		</div>
+	</div>
+	<!-- js -->
+	<script src="vendors/scripts/core.js"></script>
+	<script src="vendors/scripts/script.min.js"></script>
+	<script src="vendors/scripts/process.js"></script>
+	<script src="vendors/scripts/layout-settings.js"></script>
+	<script src="src/plugins/apexcharts/apexcharts.min.js"></script>
+	<script src="src/plugins/datatables/js/jquery.dataTables.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+	<script src="src/plugins/datatables/js/dataTables.responsive.min.js"></script>
+	<script src="src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+	<script src="vendors/scripts/dashboard.js"></script>
+</body>
+</html>
